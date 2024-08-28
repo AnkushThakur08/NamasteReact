3b9c5e7cc595ae0039456847f6b8ed9fd53767c3
Ankush Thakur <66546775+AnkushThakur08@users.noreply.github.com>
2024-08-28 06:40:33 +0000

    Assignment -1 coding question completed

--- 01_Lecture/App.js
+++ 01_Lecture/App.js
@@ -14,6 +14,8 @@
    </div>
 */
 
+
+
 const heading = React.createElement(
   "h1",
   {
---
+++ Assignments/01_Lecture/App.js
@@ -0,0 +1,41 @@
+// SIMPLE ELEMENT
+
+// const heading = React.createElement("div", {}, "Hello From React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// Nested Elements
+/*
+   <div id="parent">
+       <div id="child">
+           <h1>This is H1 Tag</h1>
+           <h2>This is H1 Tag</h2>
+       </div>
+
+       <div id="child2">
+           <h1>This is H1 Tag</h1>
+           <h2>This is H2 Tag</h2>
+       </div>
+
+
+   </div>
+
+*/
+
+const nestedElement = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is a H1 tag"),
+    React.createElement("h2", {}, "This is H2 tag"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is a H1 tag"),
+    React.createElement("h2", {}, "This is H2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(nestedElement);
